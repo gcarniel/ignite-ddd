4428b663d797b62dae97fbc0f65d68f02066b993
Gabriel Carniel <gpcarniel@outlook.com>
2023-09-09 18:30:16 -0300

    deletar comentario de uma questao

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -6,7 +6,25 @@ export class InMemoryQuestionCommentsRepository
 {
   public items: QuestionComment[] = []
 
+  async findById(id: string) {
+    const questionComment = this.items.find((item) => item.id.toString() === id)
+
+    if (questionComment) {
+      return questionComment
+    }
+
+    return null
+  }
+
   async create(questionComment: QuestionComment) {
     this.items.push(questionComment)
+  }
+
+  async delete(questionComment: QuestionComment) {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === questionComment.id,
+    )
+
+    this.items.splice(itemIndex, 1)
   }
 }
